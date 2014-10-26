eaaae0aa1ad32145060ea1c05fcba8385d0cfd0c
TimonKnigge <timonknigge@live.nl>
2014-10-26 16:19:06 +0100

    Adds comments to GCJQ2013B.

--- GCJ-Q2013-B/gcj-q2013-b.cpp
+++ GCJ-Q2013-B/gcj-q2013-b.cpp
@@ -14,7 +14,7 @@ int main(){
 		cin >> N >> M;
 
 		vector<vector<int>> board(N);
-		vector<int> rmax(N), cmax(M);
+		vector<int> rmax(N), cmax(M); // Highest value in row i / column j.
 		fill(rmax.begin(), rmax.end(), -1);
 		fill(cmax.begin(), cmax.end(), -1);
 
@@ -22,6 +22,8 @@ int main(){
 		for (int i = 0; i < N; i++){
 			board[i] = vector<int>(M);
 			for (int j = 0; j < M; j++){
+				// Add to board and update the maximum of the row, column.
+
 				cin >> val;
 				board[i][j] = val;
 
@@ -30,6 +32,8 @@ int main(){
 			}
 		}
 
+		// We can mow the lawn in the given pattern iff each number
+		// is either the maximum of its row or the maximum of its column.
 		bool canDo = true;
 		for (int i = 0; i < N; i++){
 			for (int j = 0; j < M; j++){
