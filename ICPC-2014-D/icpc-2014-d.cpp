6e4b50cf0553ed69940f20a973bcfaf050302c68
Timon Knigge <timonknigge@live.nl>
2015-02-08 13:09:17 +0100

    Adds some clarification to ICPC2014D.

--- ICPC-2014-D/icpc-2014-d.cpp
+++ ICPC-2014-D/icpc-2014-d.cpp
@@ -48,13 +48,21 @@ int main(){
 	}
 
 	vvi pToq(n, vi(n, -1));
+	// For each p, determine in how many moves we can reach each q.
 	for (int p = 0; p < n; ++p) {
+		// The only position we can reach from p in 0 moves is p itself.
 		int S = 1 << p;
 		pToq[p][p] = 0;
+		// Then, for every 1 <= it <= n (crude upperbound), determine
+		// what positions we can reach using information found for
+		// it - 1 (DP).
 		for (int it = 1; it <= n; ++it) {
 			int newS = S;
 			for (int q = 0; q < n; ++q) {
 				if (pToq[p][q] != -1) continue;
+				// Determine if there is any subset that forces the
+				// opponent to pick a position that we already know
+				// will lead to victory.
 				for (int subs : options[q]) {
 					if (setContainsSet(subs, S)) {
 						pToq[p][q] = it;
@@ -63,10 +71,12 @@ int main(){
 					}
 				}
 			}
+			// States that can be reached in it or less moves.
 			S = newS;
 		}
 	}
 
+	// Do output
 	for (int p = 0; p < n; ++p) {
 		for (int q = 0; q < n; ++q) {
 			cout << pToq[q][p] << ' ';
