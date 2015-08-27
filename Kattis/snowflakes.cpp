e88499c94dadcfb3e56bb0ec2cccc011958fe537
Timon Knigge <timonknigge@live.nl>
2015-08-28 00:39:16 +0200

    Add snowflakes.

---
+++ Kattis/snowflakes.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	vector<bool> bs(1e9+2, false);
+	
+	int T;
+	cin >> T;
+	for (int t = 1; t <= T; ++t) {
+		int n;
+		cin >> n;
+		if (n == 0) { cout << 0 << '\n'; continue; }
+		
+		int L = 0;
+		queue<int> q;
+
+		for (int r = 0; r < n; ++r) {
+			int a;
+			cin >> a;
+			
+			// Adding a might require removing elements:
+			if (bs[a]) {
+				while (q.front() != a) {
+					bs[q.front()] = false;
+					q.pop();
+				}
+				q.pop();
+			} else bs[a] = true;
+			q.push(a);
+			L = max(L, int(q.size()));
+		}
+		
+		while (!q.empty()) { bs[q.front()] = false; q.pop(); }
+		
+		cout << L << '\n';
+	}
+	cout << flush;
+	
+	return 0;
+}
