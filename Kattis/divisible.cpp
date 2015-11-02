687fa9eedcefac00e2bf6e5ae0dab8b3157756a2
Timon Knigge <timonknigge@live.nl>
2015-11-02 19:01:30 +0100

    Faster divisible.

--- Kattis/divisible.cpp
+++ Kattis/divisible.cpp
@@ -15,6 +15,8 @@
 #include <cmath>
 #include <chrono>
 #include <cassert>
+#include <unordered_map>
+#include <cstring>
 
 using namespace std;
 using ll = long long;
@@ -28,41 +30,49 @@ using vvii = vector<vii>;
 const int INF = 2000000000;
 const ll LLINF = 9000000000000000000;
 
-void solve() {
-	int _d, n;
-	scanf("%d %d", &_d, &n);
-	ll d = ll(_d);
-	vector<ll> num(n, 0LL);
-	for (int i = 0; i < n; ++i) {
-		int x;
-		scanf("%d", &x);
-		num[i] = ll(x);
-	}
-	
-	// For each position, find out whether it is the end point of a
-	// sequence divisible by d.
-	map<ll, int> cnt;
-	cnt.insert({0, 1});
-	ll ans = 0LL, sum = 0LL;
-	for (int r = 0; r < n; ++r) {
-		sum += num[r];
-		sum %= d;
-		
-		auto it = cnt.find(sum);
-		if (it == cnt.end()) cnt.insert({sum, 1});
-		else {
-			ans += ll(it->second);
-			it->second++;
-		}
-	}
-	cout << ans << endl;
+int cnt[1000000];
+
+
+void scan(int &x){	// doesn't handle negative numbers
+	char c;
+	while((x=getchar_unlocked())<'0');
+	for(x-='0'; '0'<=(c=getchar_unlocked()); x=10*x+c-'0');
 }
+void print(ll x){
+	char buf[20], *p=buf;
+	if(!x) putchar_unlocked('0');
+	else{
+		while(x) *p++='0'+x%10, x/=10;
+		do putchar_unlocked(*--p); while(p!=buf);
+	}
+}
+
+int c, d, n, x, sum;
+ll ans = 0LL;
 
 int main() {
 	
-	int c;
-	scanf("%d", &c);
-	while (c--) solve();
+	scan(c);
+	while (c--) {
+		scan(d);
+		scan(n);
+
+		// For each position, find out whether it is the end point of a
+		// sequence divisible by d.
+		memset(cnt, 0, d * sizeof(int));
+		ans = 0LL;
+		sum = 0;
+		
+		while (n--) {
+			cnt[sum]++;
+			scan(x);
+			sum = (sum + x) % d;
+			ans += cnt[sum];
+		}
+		print(ans);
+		putchar_unlocked('\n');
+	}
 	
 	return 0;
 }
+
