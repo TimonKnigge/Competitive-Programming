29041f013f1848c2ab341979d5c2113f7ce3f9fc
Timon Knigge <TimonKnigge@users.noreply.github.com>
2016-02-14 22:20:45 +0100

    Clean up p537 a bit.

--- ProjectEuler/p537.cpp
+++ ProjectEuler/p537.cpp
@@ -32,92 +32,6 @@ const ll LLINF = 9000000000000000000;
 
 constexpr ll MOD = 1004535809;
 
-/*
-inline int next_power_of_2(int x) {
-	x = (x - 1) | ((x - 1) >> 1);
-	x |= x >> 2; x |= x >> 4;
-	x |= x >> 8; x |= x >> 16;
-	return x + 1;
-}
-
-inline int brinc(int x, int k) {
-	int I = k - 1, s = 1 << I;
-	x ^= s;
-	if ((x & s) != s) {
-		I--; s >>= 1;
-		while (I >= 0 && ((x & s) == s)) {
-			x = x &~ s;
-			I--;
-			s >>= 1;
-		}
-		if (I >= 0) x |= s;
-	}
-	return x;
-}
-
-struct Complex {
-	long double u,v;
-	Complex operator+(Complex r) const { return {u+r.u, v+r.v}; }
-	Complex operator-(Complex r) const { return {u-r.u, v-r.v}; }
-	Complex operator*(Complex r) const {
-		return {u * r.u - v * r.v, u * r.v + v * r.u};
-	}
-	Complex operator/(Complex r) {
-		auto norm = r.u*r.u+r.v*r.v;
-		return {(u * r.u + v * r.v) / norm, (v * r.u - u * r.v) / norm};
-	}
-	static
-	Complex exp(complex<ld> c){ c = std::exp(c); return {c.real(), c.imag()}; }
-};
-
-#define MY_PI 3.14159265358979323846
-
-// A.size() = N = 2^p
-void fft(vector<Complex> &A, int N, int p, bool inv = false) {
-	for(int i = 0, r = 0; i < N; ++i, r = brinc(r, p))
-		if (i < r) swap(A[i], A[r]);
-	for (int m = 2; m <= N; m <<= 1) {
-		Complex w, w_m = Complex::exp(complex<ld>(0, 2*MY_PI/m*(inv?-1:1)));
-		for (int k = 0; k < N; k += m) {
-			w = {1, 0};
-			for (int j = 0; j < m / 2; ++j) {
-				Complex t = w * A[k + j + m / 2];
-				A[k + j + m / 2] = A[k + j] - t;
-				A[k + j] = A[k + j] + t;
-				w = w * w_m;
-			}
-		}
-	}
-	if (inv) for (int i = 0; i < N; ++i) {
-		A[i].u /= N; A[i].v /= N;
-	}
-}
-
-void convolution(vector<Complex> &A,vector<Complex> &B,vector<Complex> &C){
-	// Pad with zeroes
-	int N = 2 * max(next_power_of_2(A.size()), next_power_of_2(B.size()));
-	A.reserve(N); B.reserve(N); C.reserve(N);
-	for (int i = A.size(); i < N; ++i) A.push_back({0, 0});
-	for (int i = B.size(); i < N; ++i) B.push_back({0, 0});
-	int p = int(log2(N) + 0.5);
-	// Transform A and B
-	fft(A, N, p, false);
-	fft(B, N, p, false);
-	// Calculate the convolution in C
-	for (int i = 0; i < N; ++i) C.push_back(A[i] * B[i]);
-	fft(C, N, p, true);
-}
-
-void square_inplace(vector<Complex> &A) {
-	int N = 2 * next_power_of_2(A.size());
-	A.reserve(N);
-	for (int i = A.size(); i < N; ++i) A.push_back({0, 0});
-	int p = int(log2(N) + 0.5);
-	fft(A, N, p, false);
-	for (int i = 0; i < N; ++i) A[i] = A[i] * A[i];
-	fft(A, N, p, true);
-}
-*/
 constexpr ll SIZE = 3e5+10;
 bitset<SIZE + 1> bs;
 vector<ll> primes;
@@ -160,45 +74,6 @@ void bin_pow_cap_at(vector<ll> &a, int k, int cap, vector<ll> &res) {
 	}
 }
 
-/*
-void bin_pow_cap_at(vector<ll> &a, int k, int cap, vector<ll> &res) {
-	if (k == 0) {
-		res.assign(cap, 0LL);
-		res[0] = 1LL;
-	} else if (k == 1) {
-		// done!
-		res.assign(cap, 0LL);
-		for (int i = 0; i < min(int(a.size()), cap); ++i)
-			res[i] = a[i] % MOD;
-	} else if (!(k&1)) {
-		vector<Complex> A(a.size(), {0, 0}), B(a.size(), {0, 0}), C;
-		for (int i = 0; i < a.size(); ++i)
-			A[i] = B[i] = {a[i] % MOD, 0};
-		convolution(A, B, C);
-		a.assign(cap, 0LL);
-		for (int i = 0; i < min(int(C.size()), cap); ++i) {
-			C[i].u -= floor(C[i].u / MOD) * MOD;
-//			while (C[i].u > MOD * MOD) C[i].u -= MOD * MOD;
-			a[i] = ll(C[i].u + 0.5) % MOD;
-		}
-		bin_pow_cap_at(a, k / 2, cap, res);
-	} else {
-		vector<Complex> A(a.size(), {0, 0}), B, C;
-		for (int i = 0; i < a.size(); ++i)
-			A[i] = {a[i] % MOD, 0};
-		bin_pow_cap_at(a, k - 1, cap, res);
-		B.assign(res.size(), {0, 0});
-		for (int i = 0; i < res.size(); ++i)
-			B[i] = {res[i] % MOD, 0};
-		convolution(A, B, C);
-		res.assign(cap, 0);
-		for (int i = 0; i < min(int(C.size()), cap); ++i) {
-			C[i].u -= floor(C[i].u / MOD) * MOD;
-//			while (C[i].u > MOD * MOD) C[i].u -= MOD * MOD;
-			res[i] = ll(C[i].u + 0.5) % MOD;
-		}
-	}
-}*/
 
 int main() {
 	
