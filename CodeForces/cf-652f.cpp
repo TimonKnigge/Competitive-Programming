e1297fe128d447c19bf539165abe7973f60d1c55
Timon Knigge <timonknigge@live.nl>
2016-03-26 20:51:43 +0100

    Clearer version of 652F.

--- CodeForces/cf-652f.cpp
+++ CodeForces/cf-652f.cpp
@@ -29,6 +29,11 @@ using vvii = vector<vii>;
 const int INF = 2000000000;
 const ll LLINF = 9000000000000000000;
 
+// Brings v into range [1, M]
+void fix_mod(ll &v, ll M) {
+	v = ((v - 1) % M + M) % M + 1;
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -37,61 +42,44 @@ int main() {
 	ll M, T;
 	cin >> N >> M >> T;
 	
-	vi S(N, 0LL);
-	vi D(N, 0LL);
+	// tuples {starting position of ant i, i}
 	vii Si(N, {0LL, 0LL});
-	bool lf = false, rf = false;
-	int dsum = 0;
-	for (int i = 0; i < N; ++i) {
-		char c;
-		cin >> S[i] >> c;
-		D[i] = (c == 'L' ? -1 : 1);
-		if (D[i] == -1) lf = true; else rf = true;
-		dsum += D[i];
-		Si[i] = {S[i], i};
-	}
-	
+	// final positions
 	vi pos(N, 0LL);
-	for (int i = 0; i < N; ++i) {
-		pos[i] = (S[i] + D[i] * (T%M)) + M;
-		pos[i] %= M;
-		if (pos[i] == 0) pos[i] = M;
-	}
+	// Intersection points with x=1/2 for all lines for t \in [0, M)
+	// Also the slope.
+	vii intercepts(N, {0LL, 0LL});
 	
-	if (!lf || !rf) {
-		for (int i = 0; i < N; ++i)
-			cout << pos[i] << " \n"[i + 1 == N];
-		return 0;
-	}
-	
-	vii crossdir;
+	ll shift = 0;
 	for (int i = 0; i < N; ++i) {
+		ll x;
+		char c;
+		cin >> x >> c;
 		
-		ll k = - D[i] * (D[i] == 1 ? (S[i] - 1) : S[i]);
-		while (k <= 0) k += M;
-		k %= M;
-		if (k == 0) k += M;
-		crossdir.push_back({k, D[i]});
+		ll slope = (c == 'L' ? -1 : 1);
+		shift += ((T / M) % N) * slope;
+		
+		Si[i] = {x, i};
+		pos[i] = x + (T % M) * slope;
+		fix_mod(pos[i], M);
+		
+		intercepts[i] = {(slope == 1 ? 1 - x : x), slope};
+		fix_mod(intercepts[i].first, M);
 	}
 	
-	sort(crossdir.begin(), crossdir.end());
-	
-	ll shift = -(T / M) * dsum;
-	for (size_t j = 0; j < crossdir.size() && crossdir[j].first <= (T%M); ++j) {
-//		cerr << crossdir[j].first << ' ' << crossdir[j].second << endl;
-		shift -= crossdir[j].second;
-	}
-	
-	shift = ((shift % N) + N) % N;
-	
-//	cerr << shift << endl;
-	sort(pos.begin(), pos.end());
 	sort(Si.begin(), Si.end());
+	sort(pos.begin(), pos.end());
+	sort(intercepts.begin(), intercepts.end());
 	
-	vi ans(N, 0);
-	for (int i = 0; i < N; ++i) {
-		ans[Si[i].second] = pos[(i + N - shift) % N];
+	for (int i = 0; i < N && intercepts[i].first <= T % M; ++i) {
+		shift += intercepts[i].second;
 	}
+	fix_mod(shift, N);
+	
+	// Map answers back into the ordering they had in the input
+	vi ans(N, 0LL);
+	for (int i = 0; i < N; ++i)
+		ans[Si[i].second] = pos[(i + shift) % N];
 	
 	for (int i = 0; i < N; ++i)
 		cout << ans[i] << " \n"[i + 1 == N];
