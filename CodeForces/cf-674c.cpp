5792eda070988e46f419213696dd07cf7408daa8
Timon Knigge <timonknigge@live.nl>
2016-05-08 13:43:28 +0200

    Succincter version of 674c.

--- CodeForces/cf-674c.cpp
+++ CodeForces/cf-674c.cpp
@@ -29,71 +29,65 @@ using vvii = vector<vii>;
 const int INF = 2000000000;
 const ll LLINF = 9000000000000000000;
 
-void divcon(vi &t, vi &p, int k, int l, int r, int lo, int hi, vector<ld> &dp, vector<ld> &old_dp, vector<ld> &st, vector<ld> &st2) {
+void divide_and_conquer(
+	int l, int r, int lo, int hi,
+	vi &T, vi &P, vector<ld> &recip_suff, vector<ld> &prod_sum,
+	vector<ld> &dp, vector<ld> &old_dp) {
 	
 	int m = (l + r) / 2;
-	int fst = min(m, hi);
-	ld sum = st2[m] - st2[fst - 1]
-		- (p[m] - ld(fst > 0 ? p[fst - 1] : 0LL))
-		* (m + 1 < t.size() ? st[t.size() - 1] - st[m] : 0.0);
-	dp[m] = old_dp[fst - 1] + sum;
-	int A = fst;
-	for (int i = fst - 1; i >= max(k - 1, lo); --i) {
-		sum += t[i] * (st[m] - st[i - 1]);
-		if (dp[m] > old_dp[i - 1] + sum) {
+	int my_hi = min(m, hi);
+	int opt = my_hi;
+	ld sum = prod_sum[my_hi] - prod_sum[m + 1]
+		- recip_suff[m + 1]
+		* ld(P[m] - P[my_hi - 1]);
+	dp[m] = old_dp[my_hi - 1] + sum;
+	for (int i = my_hi - 1; i >= lo; --i) {
+		sum += ld(T[i]) * (recip_suff[i] - recip_suff[m + 1]);
+		if (old_dp[i - 1] + sum < dp[m]) {
 			dp[m] = old_dp[i - 1] + sum;
-			A = i;
+			opt = i;
 		}
 	}
-//	cerr << lo << ' ' << hi << ' ' << k << endl;
-//	cerr << l << ' ' << m << ' ' << r << endl;
-//	cerr << "dp[" << k - 1 << "]["<< m << "] = " << dp[m] << endl;
 	
-	if (l < m)
-		divcon(t, p, k, l, m - 1, lo, min(A, hi), dp, old_dp, st, st2);
+	if (l < m) 
+		divide_and_conquer(l, m-1, lo, opt, T, P, recip_suff, prod_sum, dp, old_dp);
 	if (m < r)
-		divcon(t, p, k, m + 1, r, max(A, lo), hi, dp, old_dp, st, st2);
+		divide_and_conquer(m+1, r, opt, hi, T, P, recip_suff, prod_sum, dp, old_dp);
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
 	
-	ll n, k;
-	cin >> n >> k;
-	vi t(n, 0LL), p(n, 0LL);
-	vector<ld> reciprocal_sum(n, 0.0);
-	for (int i = 0; i < n; ++i) {
-		cin >> t[i];
-		p[i] = t[i] + (i > 0 ? p[i - 1] : 0LL);
-		reciprocal_sum[i] = 1.0 / ld(t[i])
-			+ (i > 0 ? reciprocal_sum[i - 1] : 0.0);
+	int N, K;
+	scanf("%d %d", &N, &K);
+	vi T(N, 0LL), P(N, 0LL);
+	for (int i = 0, v; i < N; ++i) {
+		scanf("%d", &v);
+		T[i] = v;
+		P[i] = (i > 0 ? P[i - 1] : 0LL) + T[i];
 	}
 	
-	vector<ld> prodsum(n, 0.0);
-	for (int i = 0; i < n; ++i)
-		prodsum[i] = ld(t[i]) * (reciprocal_sum[n - 1] - (i > 0 ? reciprocal_sum[i - 1] : 0.0))
-			+ (i > 0  ? prodsum[i - 1] : 0.0);
+	vector<ld> recip_suff(N + 1, 0.0), prod_sum(N + 1, 0.0);
+	for (int i = N - 1; i >= 0; --i)
+		recip_suff[i] = recip_suff[i + 1] + 1.0 / ld(T[i]),
+		prod_sum[i] = prod_sum[i + 1] + T[i] * recip_suff[i];
 	
-	vector<ld> dp[2];
-	dp[0].assign(n, 0.0);
-	dp[1].assign(n, 0.0);
+	vector<vector<ld>> dp(2, vector<ld>(N, 0.0));
 	dp[0][0] = 1.0;
-	for (int i = 1; i < n; ++i) {
-		dp[0][i] = dp[0][i - 1] + ld(p[i]) / ld(t[i]);
-	}
-	
+	for (int i = 1; i < N; ++i)
+		dp[0][i] = dp[0][i - 1] + ld(P[i]) / ld(T[i]);
 	int c = 1;
-	for (int kk = 1; kk < k; ++kk) {
+	
+	for (int j = 1; j < K; ++j) {
 		
-		dp[c][kk] = kk + 1;
+		divide_and_conquer(
+			j, N - 1, j, N - 1,
+			T, P, recip_suff, prod_sum,
+			dp[c], dp[1 - c]);
 		
-		divcon(t, p, kk + 1, kk, n - 1, kk, n - 1, dp[c], dp[1 - c], reciprocal_sum, prodsum);
-		
-		c = 1-c;
+		c = 1 - c;
 	}
 	
-	printf("%.7lf\n", double(dp[1 - c][n - 1]));
-	
+	printf("%.7lf\n", double(dp[1 - c][N - 1]));
+
 	return 0;
 }
