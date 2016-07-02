cb5533260cbec4ec0b697be2f3f71c8621706d4a
Timon Knigge <timonknigge@live.nl>
2016-07-02 18:36:14 +0200

    Fixes almostunionfind.

--- Kattis/almostunionfind.cpp
+++ Kattis/almostunionfind.cpp
@@ -1,21 +1,46 @@
-#include <iostream>
+#include <iostream> 
+#include <cstdio>
+#include <string>
+#include <sstream>
 #include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <numeric>
+#include <iterator>
 #include <algorithm>
+#include <cmath>
+#include <chrono>
+#include <cassert>
 
 using namespace std;
+using ll = long long;
+using ld = long double;
+using ii = pair<ll, ll>;
+using vi = vector<ll>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using vvii = vector<vii>;
 
-class UnionFind {
-private:
-	vector<int>par, rank, size;
-	int c;
-public:
-	UnionFind(int n) : par(n), rank(n, 0), size(n, 1), c(n) {
+const int INF = 2000000000;
+const ll LLINF = 9000000000000000000;
+
+struct UnionFind {
+	vi par, rank, size; int c;
+	UnionFind(int n) : par(n), rank(n,0), size(n,1), c(n) {
 		for (int i = 0; i < n; ++i) par[i] = i;
 	}
 
 	int find(int i) { return (par[i] == i ? i : (par[i] = find(par[i]))); }
 	bool same(int i, int j) { return find(i) == find(j); }
-	void union_set(int i, int j) {
+	int get_size(int i) { return size[find(i)]; }
+	int count() { return c; }
+
+	void merge(int i, int j) {
 		if ((i = find(i)) == (j = find(j))) return;
 		c--;
 		if (rank[i] > rank[j]) swap(i, j);
@@ -32,13 +57,14 @@ int main() {
 	while (cin >> n) {
 		cin >> m;
 		UnionFind uf(n);
-		vector<int> size(n, 1), val(n, 1), set(n, 0);
+		
+		vi val(n, 1), size(n, 1), set(n, 0);
 		for (int i = 0; i < n; ++i) {
 			val[i] = i + 1;
 			set[i] = i;
 		}
-
-		for (int i = 0; i < m; ++i) {
+		
+		while (m--) {
 			int c;
 			cin >> c;
 			if (c == 1) { // Union p and q
@@ -47,26 +73,22 @@ int main() {
 				p--; q--;
 				if (uf.same(set[p], set[q])) continue;
 				int setp = uf.find(set[p]), setq = uf.find(set[q]);
-				uf.union_set(set[p], set[q]);
+				uf.merge(set[p], set[q]);
 				int new_set = uf.find(set[p]);
-//				cout << val[setp] << ' ' <<val[setq]<<endl;
-				size[new_set] = size[setp] + size[setq];
 				val[new_set] = val[setp] + val[setq];
 				set[p] = set[q] = new_set;
-
-//cerr << "Unioned set " <<setp << " containing " << p <<" of and " <<setq << " containing " << q << ", now in " << new_set << " of size " << size[new_set] << " with sum " << val[new_set] << endl;
-				
+				size[new_set] = size[setp] + size[setq];
 			} else if (c == 2) {
 				int p, q;
 				cin >> p >> q;
 				p--; q--;
 				if (uf.same(set[p], set[q])) continue;
 				int setp = uf.find(set[p]), setq = uf.find(set[q]);
-				size[setp] -= 1;
 				val[setp] -= p + 1;
+				size[setp]--;
 				set[p] = setq;
-				size[setq]++;
 				val[setq] += p + 1;
+				size[setq]++;
 			} else {
 				int p;
 				cin >> p;
@@ -74,7 +96,6 @@ int main() {
 				cout << size[uf.find(set[p])] << ' ' << val[uf.find(set[p])] << '\n';
 			}
 		}
-		cout << flush;
 	}
 	
 	return 0;
