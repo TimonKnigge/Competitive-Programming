50f1c370d004c8dd9a76ae51e40bf8962d2e098e
Timon Knigge <timonknigge@live.nl>
2017-06-03 19:52:20 +0200

    Committee assignment.

--- Kattis/committeeassignment.cpp
+++ Kattis/committeeassignment.cpp
@@ -1,26 +1,10 @@
-#include <iostream> 
-#include <cstdio>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <numeric>
-#include <iterator>
-#include <algorithm>
-#include <cmath>
-#include <chrono>
-#include <cassert>
+#include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 using ld = long double;
-using ii = pair<int,int>;
-using vi = vector<int>;
+using ii = pair<ll, ll>;
+using vi = vector<ll>;
 using vb = vector<bool>;
 using vvi = vector<vi>;
 using vii = vector<ii>;
@@ -29,95 +13,91 @@ using vvii = vector<vii>;
 const int INF = 2000000000;
 const ll LLINF = 9000000000000000000;
 
-int fmap(int &id, map<string, int> &mp, string &s) {
+template <class T>
+int getid(unordered_map<T, int> &mp, T &s) {
 	auto it = mp.find(s);
-	if (it != mp.end()) return it->second;
-	else {
+	if (it == mp.end()) {
+		int id = (int)mp.size();
 		mp.insert({s, id});
-		++id;
-		return id - 1;
-	}
+		return id;
+	} else	return it->second;
 }
 
-int recurse(int u, vvi &el, vi &col, int N, int maxC) {
-	if (u == N) return maxC;
-	
-	int mn = INF;
-	for (int c = 0; c < min(mn, N); ++c) {
-		bool f = true;
-		for (int i = 0, s = int(el[u].size()); f && i < s; ++i) {
-			f = f && c != col[el[u][i]];
+vi get_independent_sets(vi &nb) {
+	int n = (int)nb.size();
+	vi s(1<<n, 0LL);
+	s[0] = 1LL;
+	for (int i = 1; i < (1<<n); ++i) {
+		int v = 0;
+		while (((i>>v)&1) == 0) ++v;
+		s[i] = s[i&~(1<<v)] + s[i&~(nb[v]|(1<<v))];
+	}
+	return s;
+}
+
+ll pw(ll v, int k) {
+	if (k == 0) return 1LL;
+	if (k == 1) return v;
+	ll r = pw(v*v, k/2);
+	if (k&1) r *= v;
+	return r;
+}
+
+int solve(vi &nb) {
+	int n = (int)nb.size(), a = (1<<n)-1;
+	vi s = get_independent_sets(nb);
+	for (int k = 1; k < n; ++k) {
+		ll ck = 0LL;
+		for (int i = 0; i < (1<<n); ++i) {
+			ck += ((__builtin_popcount(i)&1) ? -1 : 1)
+				* pw(s[a&~i], k);
 		}
-		if (f) {
-			col[u] = c;
-			mn = min(mn, recurse(u + 1, el, col, N, max(maxC, c)));
-		}
+		if (ck > 0) return k;
 	}
-	return mn;
-}
-
-void reorder(vvi &e) {
-	int N = int(e.size());
-	
-	int u = 0;
-	for (int v = 1; v < N; ++v)
-		if (e[u].size() < e[v].size()) u = v;
-	
-	vi M(N, -1);
-	int id = 0;
-	
-	stack<int> q;
-	for (int i = 0; i < N; ++i) if (e[i].size() > 0) q.push(i);
-	q.push(u);
-	
-	while (!q.empty()) {
-		int a = q.top();
-		q.pop();
-		if (M[a] >= 0) continue;
-		M[a] = id;
-		++id;
-		for (int b : e[a]) if (M[b] < 0) q.push(b);
-	}
-	vvi e2(id, vi());
-	
-	for (int a = 0; a < N; ++a)
-		for (int b : e[a])
-			if (M[a]>M[b]) e2[M[a]].push_back(M[b]);
-	e = e2;
-}
-
-bool solve() {
-	int N, M;
-	cin >> N >> M;
-	if (N == 0 && M == 0) return false;
-	
-	int id = 0;
-	map<string, int> idmap;
-	vvi el(N, vi());
-	
-	while (M--) {
-		string n1, n2;
-		cin >> n1 >> n2;
-		int id1 = fmap(id, idmap, n1);
-		int id2 = fmap(id, idmap, n2);
-		el[id1].push_back(id2);
-		el[id2].push_back(id1);
-	}
-//	cerr << "Read names." << endl;
-	reorder(el);
-//	cerr << "Reordered."<<endl;
-	
-	vi col(N, -1);
-	cout << (1 + recurse(0, el, col, int(el.size()), 0)) << endl;
-	
-	return true;
+	return n;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	while (solve()) ;
+	int n, m;
+	while (cin >> n >> m) {
+		if (n == 0 && m == 0) break;
+		
+		unordered_map<string, int> mp;
+		vvi E(n, vi());
+		while (m--) {
+			string l, r;
+			cin >> l >> r;
+			int u = getid<string>(mp, l), v = getid<string>(mp, r);
+			E[u].push_back(v);
+			E[v].push_back(u);
+		}
+		
+		int ans = 1;
+		vb vis(n, false);
+		for (int i = 0; i < n; ++i) {
+			if (vis[i]) continue;
+			unordered_map<int, int> mp2;
+			queue<int> q; q.push(i);
+			while (!q.empty()) {
+				int u = q.front(); q.pop();
+				mp2.insert({u, (int)mp2.size()});
+				vis[u] = true;
+				for (int v : E[u]) if (!vis[v]) q.push(v);
+			}
+			
+			vi nb(mp2.size(), 0LL);
+			for (ii pr : mp2) {
+				int u = pr.first, id = pr.second;
+				for (int v : E[u])
+					nb[id] |= (1LL << getid<int>(mp2, v));
+			}
+			ans = max(ans, solve(nb));
+		}
+		cout << ans << endl;
+	}
 	
 	return 0;
 }
